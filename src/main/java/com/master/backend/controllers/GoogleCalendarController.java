b31ae7a4ab6e8819c787d1f306b86b8c463bd94d
Corina Strachinariu <corina-andreea.strachinariu@sap.com>
2020-04-25 16:21:52 +0300

    google calendar integration

---
+++ src/main/java/com/master/backend/controllers/GoogleCalendarController.java
@@ -0,0 +1,136 @@
+package com.master.backend.controllers;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.*;
+import com.master.backend.entities.Booking;
+import com.master.backend.entities.User;
+import com.master.backend.repositories.BookingRepository;
+import com.master.backend.repositories.UserRepository;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.*;
+
+@CrossOrigin("*")
+@Controller
+public class GoogleCalendarController {
+    private final static Log logger = LogFactory.getLog(GoogleCalendarController.class);
+    private static final String APPLICATION_NAME = "";
+    private static HttpTransport httpTransport;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static Calendar client;
+
+    private GoogleClientSecrets clientSecrets;
+    private GoogleAuthorizationCodeFlow flow;
+    private Credential credential;
+
+    @Value("${google.client.client-id}")
+    private String clientId;
+    @Value("${google.client.client-secret}")
+    private String clientSecret;
+    @Value("${google.client.redirectUri}")
+    private String redirectURI;
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/login/google", method = RequestMethod.GET)
+    public RedirectView googleConnectionStatus(HttpServletRequest request) throws Exception {
+        return new RedirectView(authorize());
+    }
+
+    @RequestMapping(value = "/login/google", method = RequestMethod.GET, params = "code")
+    public String oauth2Callback(@RequestParam(value = "code") String code) {
+        try {
+            TokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectURI).execute();
+            credential = flow.createAndStoreCredential(response, "userID");
+            client = new Calendar.Builder(httpTransport, JSON_FACTORY, credential)
+                    .setApplicationName(APPLICATION_NAME).build();
+            Optional<User> user = userRepository.findById(1L);
+            List<Booking> bookings = user.isPresent() ? bookingRepository.getBookingByEmployee_IdAndIsSavedGoogleFalse(user.get().getUserLoggedInId()) : new ArrayList<>();
+            createEvent(bookings);
+        } catch (Exception e) {
+            logger.warn("Exception while handling OAuth2 callback (" + e.getMessage() + ")."
+                    + " Redirecting to google connection status page.");
+        }
+
+        return "home";
+    }
+
+    private String authorize() throws Exception {
+        AuthorizationCodeRequestUrl authorizationUrl;
+        if (flow == null) {
+            GoogleClientSecrets.Details web = new GoogleClientSecrets.Details();
+            web.setClientId(clientId);
+            web.setClientSecret(clientSecret);
+            clientSecrets = new GoogleClientSecrets().setWeb(web);
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
+                    Collections.singleton(CalendarScopes.CALENDAR)).build();
+        }
+        authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectURI);
+        System.out.println("cal authorizationUrl->" + authorizationUrl);
+        return authorizationUrl.build();
+    }
+
+    private void createEvent(List<Booking> bookings) throws IOException {
+        for(Booking booking: bookings) {
+            Event event = new Event()
+                    .setSummary(booking.getSubject())
+                    .setLocation(booking.getMeetingRoom().getName())
+                    .setDescription(booking.getSubject());
+
+            DateTime startDateTime = new DateTime(booking.getBookedFrom());
+            EventDateTime start = new EventDateTime()
+                    .setDateTime(startDateTime)
+                    .setTimeZone("GMT+03:00");
+            event.setStart(start);
+
+            DateTime endDateTime = new DateTime(booking.getBookedUntil());
+            EventDateTime end = new EventDateTime()
+                    .setDateTime(endDateTime)
+                    .setTimeZone("GMT+03:00");
+            event.setEnd(end);
+
+//        EventAttendee[] attendees = new EventAttendee[] {
+//                new EventAttendee().setEmail("lpage@example.com"),
+//                new EventAttendee().setEmail("sbrin@example.com"),
+//        };
+//        event.setAttendees(Arrays.asList(attendees));
+
+            String calendarId = "primary";
+            event = client.events().insert(calendarId, event).execute();
+            System.out.printf("Event created: %s\n", event.getHtmlLink());
+
+            booking.setSavedGoogle(true);
+            bookingRepository.save(booking);
+        }
+    }
+}
